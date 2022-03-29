a3afbd94b7a96ce8df7c1a3999b78b895ce4bed2
Xlyinpin <76021258+Xlyinpin@users.noreply.github.com>
2022-03-29 14:08:55 +0800

    Update qyxj.js

--- qyxj.js
+++ qyxj.js
@@ -10,13 +10,14 @@
 
 
 
-本地重写
+[rewrite_local]
 
 https://(commerce-.*api|pay).(faceu|wecut).(com|mobi)/(commerce|apple)/(iosAppVerifyReceipt.php|v1/subscription/user_info) url script-response-body https://raw.githubusercontent.com/Xlyinpin/QuantumultX-Script/Vip-Crack/qyxj.js
 
 
 
-主机名MITM
+
+[mitm]
 
 commerce-i18n-api.faceu.mobi,commerce-api.faceu.mobi, pay.wecut.com
 
