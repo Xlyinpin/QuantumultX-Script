5b74c0bdff11e5605b4cb16860cb9674da8d031c
Xlyinpin <76021258+Xlyinpin@users.noreply.github.com>
2022-03-29 14:24:54 +0800

    Update qyxj.js

--- qyxj.js
+++ qyxj.js
@@ -1,24 +1,15 @@
 /*
 
-公众号：搞鸡玩家
-
-苹果商店下载: 轻颜相机 蒸汽波相机 （二合一解锁VIP）
-
-修改内容：解锁永久会员 
-
-注意事项：每次解锁需启动圈叉
 
 
 
 [rewrite_local]
-
-^https://(commerce-.*api|pay).(faceu|wecut).(com|mobi)/(commerce|apple)/(iosAppVerifyReceipt.php|v1/subscription/user_info) url script-response-body https://raw.githubusercontent.com/Xlyinpin/QuantumultX-Script/Vip-Crack/qyxj.js
+^https:\/\/(commerce-.*api|pay)\.(faceu|wecut)\.(com|mobi)\/(commerce|apple)\/(iosAppVerifyReceipt.php|v1\/subscription\/user_info) url script-response-body https://raw.githubusercontent.com/Xlyinpin/QuantumultX-Script/Vip-Crack/qyxj.js
 
 
 
 
 [mitm]
-
 hostname = commerce-i18n-api.faceu.mobi,commerce-api.faceu.mobi, pay.wecut.com
 
 
