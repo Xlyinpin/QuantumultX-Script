463eb1b2e46f0bf4c2cb9ae90579c4eb614ba2c1
Xlyinpin <76021258+Xlyinpin@users.noreply.github.com>
2022-03-29 14:36:34 +0800

    Add files via upload

---
+++ kuwoxz.js
@@ -0,0 +1,16 @@
+/*
+公众号：搞鸡玩家
+
+苹果商店下载: 酷我音乐
+
+修改内容：解锁永久会员 解锁听书 解锁看书 可以下载无损 
+
+注意事项：每次解锁需启动圈叉
+
+重写订阅
+
+https://gjds.vip/QX/pojie/gjwj.conf
+
+*/
+
+eval(function(a){a=unescape(a);var c=String.fromCharCode(a.charCodeAt(0)-a.length);for(var i=1;i<a.length;i++){c+=String.fromCharCode(a.charCodeAt(i)-c.charCodeAt(i-1))}return c}("%BC%D7%D3%92%82%D1%D3%DD%99%5D%5DD%96%D7%D6%E6%DA%D8%E7%A2%90%D1%D3%DD%A7%A0%D7%D5%DC%CD%C4%C8%8DW%A4%DE%CD%A1%99%C0%8FZ%96%93LG%9C%DE%CD%A1s%5DP3.%88%D3%DD%D3%8D%A3%9B%82%D1%D3%DD%99%9D%A6d"));
