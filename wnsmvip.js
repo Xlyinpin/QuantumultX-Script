e88648435b1aed88f18558490c8ac88776cb630c
Xlyinpin <76021258+Xlyinpin@users.noreply.github.com>
2022-04-03 22:31:49 +0800

    Update wnsmvip.js

--- wnsmvip.js
+++ wnsmvip.js
@@ -13,8 +13,9 @@
 
 [rewrite_local] 
 ^https:\/\/snailsleep\.net\/snail\/v1\/profile\/get url script-response-body https://raw.githubusercontent.com/Xlyinpin/QuantumultX-Script/Vip-Crack/wnsmvip.js
+^https:\/\/*.*.*.*/snail\/v1\/profile\/get url script-response-body https://raw.githubusercontent.com/Xlyinpin/QuantumultX-Script/Vip-Crack/wnsmzt.js
 [mitm]
-hostname = snailsleep.net
+hostname = snailsleep.net,*.snailsleep.net
 *
 *
 */
