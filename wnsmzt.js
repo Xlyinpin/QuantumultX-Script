16b9d2ceb6930c744692d69310c7ebe329916b13
Xlyinpin <76021258+Xlyinpin@users.noreply.github.com>
2022-04-03 22:18:34 +0800

    Add files via upload

---
+++ wnsmzt.js
@@ -0,0 +1,28 @@
+/*
+ *
+ *
+
+
+公众号：搞鸡玩家
+
+苹果商店下载: 蜗牛睡眠
+
+修改内容：解锁永久会员 
+
+注意事项：每次解锁需启动圈叉
+
+[rewrite_local] 
+^https:\/\/challenge\.snailsleep\.net\/snail-challenge\/vipActive\/canJoin url script-response-body 
+[rewrite_local] 
+^https:\/\/*.*.*.*\/snail\/v1\/profile\/get url script-response-body 
+[mitm]
+hostname = challenge.snailsleep.net
+*
+*
+*/
+var objc = JSON.parse($response.body);
+
+objc.data["status"] = "405";
+objc.data["message"] = "active-not-exists";
+
+$done({body : JSON.stringify(objc)});
