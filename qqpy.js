f188fedeb7e9c7660f108009983ef950df6ce954
Xlyinpin <76021258+Xlyinpin@users.noreply.github.com>
2022-04-03 21:27:12 +0800

    Add files via upload

---
+++ qqpy.js
@@ -0,0 +1,26 @@
+/*
+ *
+ *
+
+
+公众号：搞鸡玩家
+
+苹果商店下载: 千千配音
+
+修改内容：解锁永久会员 
+
+注意事项：每次解锁需启动圈叉
+
+[rewrite_local] 
+^https:\/\/qianqianapi\.chanwind\.com\/v1\/user/* url script-response-body 
+[mitm]
+hostname = qianqianapi.chanwind.com
+*
+*
+*/
+var objc = JSON.parse($response.body);
+
+objc.data["username"] = "公众号搞鸡玩家";
+objc.data["isVip"] = 1;
+
+$done({body : JSON.stringify(objc)});
