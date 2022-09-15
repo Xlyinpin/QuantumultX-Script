8867667913188593c300bd139f89287e890ffd1f
Xlyinpin <76021258+Xlyinpin@users.noreply.github.com>
2022-09-15 16:56:42 +0800

    Update Ylgy.js

--- Ylgy.js
+++ Ylgy.js
@@ -1,18 +1,12 @@
 /*
  *
  *
-脚本功能：鲸鱼视频无限观看
-软件版本：极速版
-下载地址：https://6478726.com/q4r3p.html?code=9ddU2bd3fe
+脚本功能：微信小程序-羊了个羊破解通关次数
 脚本作者：凉意
-更新时间：2022.02.18
-电报频道：https://t.me/liangyiA
-问题反馈：https://t.me/LiangYi66
+更新时间：2022.09.15
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
-[rewrite_local]
-^https:\/\/api\.8897815\.com\/long_video\/advertising url response-body LOGIN_BEFOR response-body 1
 [rewrite_local] 
 ^https:\/\/api\.8897815\.com\/long_video\/user\/info url script-response-body https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/jysp.js
 [mitm]
