9737d1006b1f610c9dbe99d9b77bbf6612bb0524
Xlyinpin <76021258+Xlyinpin@users.noreply.github.com>
2022-09-15 17:01:44 +0800

    Update Ylgy.js

--- Ylgy.js
+++ Ylgy.js
@@ -2,22 +2,20 @@
  *
  *
 脚本功能：微信小程序-羊了个羊破解通关次数
-脚本作者：凉意
+脚本作者：耳丶听爱情
 更新时间：2022.09.15
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
-[rewrite_local] 
-^https:\/\/api\.8897815\.com\/long_video\/user\/info url script-response-body https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/jysp.js
-[mitm]
-hostname = api.8897815.com
+[rewrite_local]
+^http[s]s:\/\/cat-match\.easygame2021\.com\/sheep\/v1\/game\/personal_info url script-response-body wf.js
+
 *
 *
 */
 var objc = JSON.parse($response.body);
 
-objc.data["nickName"] = "凉意";
-objc.data["vipEndTime"] = "2099-09-28";
-objc.data["userType"] = 1;
+objc.data["daily_count"] = "9999999999999999999";
+objc.data["topic_count"] = "9999999999999999999";
 
 $done({body : JSON.stringify(objc)});
